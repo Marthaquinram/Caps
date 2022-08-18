03fac144d0cf7433235cb100e27f3fc33c950693
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-08-18 15:21:51 -0700

    added driver and vendor test files.

---
+++ __tests__/caps.test.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+// Write unit tests for each event handler function (not event triggers themselves).
+// Use spies to help testing your logger methods(assert that console.log was called right).
+// const { pickUp, inTransit, delivered } = require('../src/driver');
+
+// const payload = {
+
+// }
